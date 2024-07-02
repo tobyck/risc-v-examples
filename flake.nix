5f6db0da86994bdd356fc9853a8ba086599fcd51
tobyck <toby_ck@icloud.com>
2024-07-02 17:33:08 +1200

    Fix indentation in flake

--- flake.nix
+++ flake.nix
@@ -1,14 +1,14 @@
 {
-  description = "Attempts at learning RISC-V assembly";
+	description = "Attempts at learning RISC-V assembly";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+	inputs = {
+		nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
 		flake-utils.url = "github:numtide/flake-utils";
-  };
+	};
 
-  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system: let
-    pkgs = import nixpkgs { inherit system; };
-  in {
+	outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system: let
+		pkgs = import nixpkgs { inherit system; };
+	in {
 		devShell = let
 			riscv-pkgs = import <nixpkgs> {
 				crossSystem = (import <nixpkgs/lib>).systems.examples.riscv32;
